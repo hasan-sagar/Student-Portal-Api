0d7636f49b692fc2be5db009e0d0cad7543893d3
hasan-sagar <khsagar0512@gmail.com>
2023-08-08 14:43:48 +0600

    Create db table and implement jwt and guard

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { JwtStrategy } from './jwt-roles/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: "munshiredwanpritomsagar",
+      signOptions: { expiresIn: '2d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class CoreModule {}
